808c2688cef49f26bb4db5fe07060f5fecc1b9e2
liubaogang <liubaogang@gmail.com>
2018-12-13 19:56:08 +0800

    Refactor.

--- graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/schema/impl/GraphQLJpaSimpleMutationDataFetcher.java
+++ graphql-jpa-query-schema/src/main/java/com/introproventures/graphql/jpa/query/schema/impl/GraphQLJpaSimpleMutationDataFetcher.java
@@ -103,42 +103,8 @@ public class GraphQLJpaSimpleMutationDataFetcher extends QraphQLJpaBaseDataFetch
     }
 
     private void setBasicTypeFieldValue(Object bean, java.lang.reflect.Field field, Object fieldValue) {
-        //FIXME: Should use Joda lib which can handle Date type well.
-        String[] pattern = new String[]{"yyyy-MM", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss"};
-
-        try {
-            //FIXME: Should not use 'field.getName().equals("id")' to check whether it is identity field.
-            if (String.class.isAssignableFrom(field.getType()) && !field.getName().equals("id")) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, fieldValue);
-            } else if (Date.class.isAssignableFrom(field.getType())) {
-                if (fieldValue.toString().contains("Z")) {
-                    fieldValue = fieldValue.toString().substring(0, 10);
-                }
-                Date dateValue = DateUtils.parseDate(fieldValue.toString(), pattern);
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, dateValue);
-            } else if (Integer.class.isAssignableFrom(field.getType())) {
-                //FIXME: Should use code similar to QraphQLJpaBaseDataFetcher::convertValue() to set field values.
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createInteger(fieldValue.toString()));
-            } else if (Boolean.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, BooleanUtils.toBooleanObject(fieldValue.toString()));
-            } else if (BigDecimal.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createBigDecimal(fieldValue.toString()));
-            } else if (Double.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createDouble(fieldValue.toString()));
-            } else if (Float.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createFloat(fieldValue.toString()));
-            } else if (Long.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createLong(fieldValue.toString()));
-            } else if (Short.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createInteger(fieldValue.toString()).shortValue());
-            } else if (Byte.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createInteger(fieldValue.toString()).byteValue());
-            } else if (BigInteger.class.isAssignableFrom(field.getType())) {
-                MyFieldUtils.writeFieldUsingSetterMethod(bean, field, NumberUtils.createBigInteger(fieldValue.toString()));
-            }
-        } catch (ParseException e) {
-            throw new IllegalArgumentException(e);
-        }
+        //TODO: Need to add test cases to test each possible type of java.lang.reflect.Field.
+        MyFieldUtils.writeFieldUsingSetterMethod(bean, field, fieldValue);
     }
 
     private boolean isPrimitiveOrWrapperOrOtherBasicType (Class<?> clazz) {
